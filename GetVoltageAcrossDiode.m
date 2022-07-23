2f4f3c0d27855d37f71c4ea172d9ccab4bb511f0
Athul Shaji <athulshaji.research@gmail.com>
2022-07-23 20:16:57 +0530

    modified commit

--- GetVoltageAcrossDiode.m
+++
@@ -1,3 +0,0 @@
-function VOLTAGE = GetVoltageAcrossDiode(I,I0,eta,Vth)
-    VOLTAGE = eta*Vth*log((I/I0)+1);
-end
--- mysum.m
+++
@@ -1,9 +0,0 @@
-function mysum
-clc;clear;close all;format short
-c=b+1;
-disp(['c=',num2str(c)]);
-end
-
-function z=b
-z=1;
-end
